74ee8c0e9640a42acfcf71f95f195088c4be16f5
danisanchezpoell <41333685+danisanchezpoell@users.noreply.github.com>
2018-08-21 11:38:24 +0200

    Revert "Merge branch 'master' of https://github.com/danisanchezpoell/PokeGame"
    This reverts commit ebce1348622b58a149c97a452fa31bcbb36e01cb, reversing
    changes made to 75c84ac7be52a743199ff3f84f7c962654349cb1.

--- poke/TurnTime.gd
+++ poke/TurnTime.gd
@@ -4,16 +4,13 @@ extends Timer
 # var a = 2
 # var b = "textvar"
 
-#func _ready():
-#	var time_left
-#	time_left = 1200
+func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-#	pass
+	pass
 
-#func _process(delta):
-	
-#	print(time_left)
+func _process(delta):
+	print (time_left)
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
